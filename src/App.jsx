e7f0516380486043141bcc60fecad1cd28e3080f
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-09-18 20:16:43 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { DateCountdown } from "./counter";
 function App() {
   return (
     <div className="timer">
-      &#129505; &#129310;
+      <div>&#129505; &#129310;</div>
       <DateCountdown
         dateTo="September 24, 2022 00:00:00 GMT-03:00"
         callback={() => alert("Hello")}
       />
-      &#129310; &#129505;
+      <div>&#129310; &#129505;</div>
     </div>
   );
 }

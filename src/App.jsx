d3d19fb46393b420a3ba553e49bbda38fe43db5e
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-09-23 14:07:10 -0300

    commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { CounterDate } from "./CounterDate";
 import { HappyBirthday } from "./HappyBirthday";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/happy-birthday-my-lov/" element={<CounterDate />} />
-        <Route
-          path="/happy-birthday-my-lov/congratulations"
-          element={<HappyBirthday />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/happy-birthday-my-lov" element={<CounterDate />} />
+      <Route
+        path="/happy-birthday-my-lov/congratulations"
+        element={<HappyBirthday />}
+      />
+    </Routes>
   );
 }

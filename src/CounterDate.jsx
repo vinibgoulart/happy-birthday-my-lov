cdf65c5a60a0298337082417980f152e42cd1915
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-09-23 12:05:14 -0300

    teste

--- src/CounterDate.jsx
+++ src/CounterDate.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { DateCountdown } from "./counter";
 
 export function CounterDate() {
+  const navigate = useNavigate();
+  const [displayButton, setDisplayButton] = useState("none");
+
+  const handleHappyBirthday = () => {
+    navigate("/happy-birthday-my-lov/congratulations");
+    navigate(0);
+  };
+
   return (
     <div className="timer">
       <div>&#129505; &#129310;</div>
       <DateCountdown
-        dateTo="September 02, 2022 00:00:00 GMT-03:00"
-        callback={() =>
-          refreshPage(
-            "http://http://127.0.0.1:5173/happy-birthday-my-lov/congratulations"
-          )
-        }
+        dateTo="September 23, 2022 10:35:30 GMT-03:00"
+        callback={() => {
+          setDisplayButton("inline");
+        }}
       />
       <div>&#129310; &#129505;</div>
+
+      <button
+        style={{ display: displayButton }}
+        onClick={() => handleHappyBirthday()}
+      >
+        Happy Birthday my Lov &#129505;
+      </button>
     </div>
   );
 }
